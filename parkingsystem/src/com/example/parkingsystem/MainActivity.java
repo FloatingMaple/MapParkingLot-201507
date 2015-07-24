ee9d2ad9c75aeefcdb8b44013912299d078bc532
FloatingMaple <1048325553@qq.com>
2015-07-24 21:27:29 +0800

    成功定位
    2015年7月24日21:27:05  之前因为没有使用opinion所以不能定位

--- parkingsystem/src/com/example/parkingsystem/MainActivity.java
+++ parkingsystem/src/com/example/parkingsystem/MainActivity.java
@@ -1,38 +1,110 @@
 package com.example.parkingsystem;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.LocationClientOption.LocationMode;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 private MapView mMapView;
 private BaiduMap baiduMap;
-	
+
+//定位相关
+private LocationClient mLocationClient;
+private MyLocationListener mLocationListener;
+private boolean isFirstIn = true;
+private Context context;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		SDKInitializer.initialize(getApplicationContext());
 		setContentView(R.layout.activity_main);
+		this.context = this;
 		initview();
+		
+		//初始化定位
+		initlocation();
 	}
+
 
 	private void initview() {
-		// TODO Auto-generated method stub
 		mMapView = (MapView) findViewById(R.id.bmapview);
+		baiduMap = mMapView.getMap();
+		//普通模式地图
+		baiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+		//设置初始的比例尺约为500m
+		MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+		baiduMap.setMapStatus(msu);
+	}
+	
+	private void initlocation() {
+		mLocationClient = new LocationClient(this);
+		mLocationListener = new MyLocationListener();
+		mLocationClient.registerLocationListener(mLocationListener);
+		
+		LocationClientOption option = new LocationClientOption();
+		option.setCoorType("bd09ll");	//坐标类型
+		option.setIsNeedAddress(true);	//设置需要地址
+		option.setOpenGps(true);		//使用GPS
+		option.setScanSpan(1000);		//定位请求间隔
+	
+		/*  关于定位模式与精度:
+		option.setLocationMode(LocationMode.Hight_Accuracy);
+		默认高精度
+		Hight_Accuracy 高精度定位模式：这种定位模式下，会同时使用网络定位和GPS定位，优先返回最高精度的定位结果；
+		Battery_Saving 低功耗定位模式：这种定位模式下，不会使用GPS，只会使用网络定位（Wi-Fi和基站定位）；
+		Device_Sensors 仅用设备定位模式：这种定位模式下，不需要连接网络，只使用GPS进行定位，这种模式下不支持室内环境的定位。
+		*/
+		mLocationClient.setLocOption(option);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private class MyLocationListener implements BDLocationListener{
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			MyLocationData data = new MyLocationData.Builder()//
+					.accuracy(location.getRadius())//
+					.latitude(location.getLatitude())//
+					.longitude(location.getLongitude())//
+					.build();
+			baiduMap.setMyLocationData(data);
+			
+			//如果是第一次使用，进行定位
+			if (isFirstIn) {
+				LatLng latLng = new LatLng(location.getLatitude(), 
+						location.getLongitude());
+				MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+				baiduMap.animateMapStatus(msu);
+				isFirstIn = false;
+				
+				Toast.makeText(context, location.getAddrStr(),Toast.LENGTH_SHORT).show();
+			}
+		}	
 	}
+	
+	@Override
+		protected void onStart() {
+			super.onStart();
+			baiduMap.setMyLocationEnabled(true);
+			mLocationClient.start();
+		}
 	
 	@Override
 		protected void onResume() {
@@ -49,6 +121,13 @@ private BaiduMap baiduMap;
 		}
 	
 	@Override
+		protected void onStop() {
+			super.onStop();
+			baiduMap.setMyLocationEnabled(false);
+			mLocationClient.stop();
+		}
+	
+	@Override
 		protected void onDestroy() {
 			// TODO Auto-generated method stub
 			mMapView.onDestroy();
@@ -57,6 +136,12 @@ private BaiduMap baiduMap;
 		}
 	
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -69,4 +154,5 @@ private BaiduMap baiduMap;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
 }
